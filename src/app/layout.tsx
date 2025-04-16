15f8012cade07c785af7c8caa086e521c01ec723
jamie <level7jamie@gmail.com>
2025-04-16 14:56:28 +0100

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Casebook",
+  description: "Tool enabling caseworkers to keep track of their tasks.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-semibold">This is the home page</h1>
+    </>
+  );
+}
