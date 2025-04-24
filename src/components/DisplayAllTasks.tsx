55ac54f34255ab3e729434647ce8d355b5880dae
jamie <level7jamie@gmail.com>
2025-04-24 19:05:15 +0100

    Add basic task creation form

--- src/components/DisplayAllTasks.tsx
+++ src/components/DisplayAllTasks.tsx
@@ -1,20 +1,23 @@
 import { GetTasks } from "@/utils/actions";
+import CreateTaskForm from "./CreateTaskForm";
 // Component that displays all tasks associated with the logged in user
 type Task = {
   id: number;
   title: string;
   description?: string;
-  status: string;
+  status: "pending" | "progress" | "complete";
   due: Date;
+  user_id: number;
 };
 export default async function DisplayAllTasks() {
-  //   const tasks = (await db.query()).rows;
-  const tasks: Task[] | null = await GetTasks();
+  const tasks: Task[] | number = await GetTasks();
+  const userId: number = typeof tasks != "number" ? tasks[0].user_id : tasks;
+  console.log("userId being returned by tasks is: ", userId);
 
   return (
     <>
       <h2>Tasks go here</h2>
-      {tasks ? (
+      {typeof tasks != "number" ? (
         <div>
           {tasks.map((task: Task) => {
             return (
@@ -27,6 +30,7 @@ export default async function DisplayAllTasks() {
                 <p>
                   <span>Due:</span> {task.due.toDateString()}
                 </p>
+                {/* Add link to view/edit task */}
               </div>
             );
           })}
@@ -34,6 +38,7 @@ export default async function DisplayAllTasks() {
       ) : (
         "No tasks yet I'm afraid! Please add a task via the link below."
       )}
+      <CreateTaskForm userId={userId} />
     </>
   );
 }
